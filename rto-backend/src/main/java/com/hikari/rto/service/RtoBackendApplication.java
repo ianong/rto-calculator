bc38de7b0e9c8977b7abeae421d4a7a553b16b82
Ian Ong <ian.ong@autodata.net>
2018-04-10 22:01:20 -0600

    initial commit

---
+++ rto-backend/src/main/java/com/hikari/rto/service/RtoBackendApplication.java
@@ -0,0 +1,12 @@
+package com.hikari.rto.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtoBackendApplication.class, args);
+	}
+}
---
+++ rto-backend/src/test/java/com/hikari/rto/service/RtoBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hikari.rto.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RtoBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
